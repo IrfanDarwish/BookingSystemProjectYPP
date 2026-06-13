ba635c5d4dd49f1df362265cc92e87f36d1d7813
IrfanDarwish <epunkd14@gmail.com>
2026-06-14 01:23:58 +0800

    fix: code cleanup

--- src/main/java/com/example/bookingsystem/controller/EventController.java
+++ src/main/java/com/example/bookingsystem/controller/EventController.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
